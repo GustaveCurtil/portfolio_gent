bc37d0e0e22484e31b02af8344f22a8c737ddfe6
Gustave Curtil <gustave.curtil@tutanota.com>
2024-06-17 12:58:05 +0200

    pdf bladerbaar

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let tabs = document.querySelectorAll(".tab");
 let pages = document.querySelectorAll("main");
+let linksblad = document.querySelector('.linksblad img');
+let rechtsblad = document.querySelector('.rechtsblad img');
+let pagina = 0;
 
 for (let i = 0; i < tabs.length; i++) {
     const tab = tabs[i];
@@ -15,13 +18,36 @@ for (let i = 0; i < tabs.length; i++) {
     })
 }
 
-let copyButtons = document.querySelectorAll(".text button");
 
-copyButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        console.log(button.id)
-        navigator.clipboard.writeText(button.id);
-        alert("je hebt '" + button.id + "' gekopieerd")
-    })
-});
 
+rechtsblad.addEventListener('click', e => {
+    if (pagina < 24) {
+        pagina += 2;
+        updatePagina(pagina);
+    }
+    console.log(pagina)
+})
+
+linksblad.addEventListener('click', e => {
+    if (pagina > 0) {
+        pagina -= 2;
+        updatePagina(pagina);
+    }
+    console.log(pagina)
+})
+
+function updatePagina(pagina){
+    if (pagina == 0) {
+        linksblad.style.opacity = '0';
+        rechtsblad.src = "portfolio/Portfolio Gustave Curtil_page-000" + (pagina + 1) + ".webp";
+    } else if (pagina < 10) {
+        linksblad.style.opacity = '1';
+        linksblad.src = "portfolio/Portfolio Gustave Curtil_page-000" + pagina + ".webp";
+        rechtsblad.src = "portfolio/Portfolio Gustave Curtil_page-000" + (pagina + 1) + ".webp";
+    } else {
+        linksblad.style.opacity = '1';
+        linksblad.src = "portfolio/Portfolio Gustave Curtil_page-00" + pagina + ".webp";
+        rechtsblad.src = "portfolio/Portfolio Gustave Curtil_page-00" + (pagina + 1) + ".webp";
+
+    } 
+}
